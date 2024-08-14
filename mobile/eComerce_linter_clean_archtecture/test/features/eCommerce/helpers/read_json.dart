fe752dbcbdcef1c085fb7455294de380d1e510f7
yohannesalex <yohannesalemayehu624@gmail.com>
2024-08-14 18:07:44 +0300

    remove some debug statments

--- mobile/eComerce_linter_clean_archtecture/test/features/eCommerce/helpers/read_json.dart
+++ mobile/eComerce_linter_clean_archtecture/test/features/eCommerce/helpers/read_json.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 String readJson(String name) {
-  print('reading jsn');
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');

24b9f96456fac7e454f5ac0436e5249220609ff6
yohannesalex <yohannesalemayehu624@gmail.com>
2024-08-19 18:04:58 +0300

    structuring the folders and files for auth

---
+++ mobile/eComerce_linter_clean_archtecture/lib/features/authentication/domain/entity/user.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final String id;
+  final String userName;
+  final String email;
+  final double password;
+
+  const UserEntity({
+    required this.id,
+    required this.userName,
+    required this.email,
+    required this.password,
+  });
+  @override
+  List<Object?> get props => [id, userName, email, password];
+}

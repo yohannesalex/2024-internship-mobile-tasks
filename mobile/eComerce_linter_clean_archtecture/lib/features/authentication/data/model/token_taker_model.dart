26b8addbcd88ccfe081769dc030d4d0e976831de
yohannesalex <yohannesalemayehu624@gmail.com>
2024-08-21 23:13:54 +0300

    feat: finish task 22 lib/features/authentication/data/model/token_taker_model.dart

---
+++ mobile/eComerce_linter_clean_archtecture/lib/features/authentication/data/model/token_taker_model.dart
@@ -0,0 +1,13 @@
+import '../../domain/entity/token_taker_entity.dart';
+
+class TokenTakerModel extends TokenTakerEntity {
+  const TokenTakerModel({required super.token});
+  factory TokenTakerModel.fromJson(Map<String, dynamic> json) {
+    return TokenTakerModel(token: json['access_token']);
+  }
+  static Map<String, dynamic> toJson(TokenTakerModel token) {
+    return {
+      'access_token': token,
+    };
+  }
+}

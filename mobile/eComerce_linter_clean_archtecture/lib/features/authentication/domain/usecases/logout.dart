c72f10e859d7a40bb81c53f81b847d1f3a467620
yohannesalex <yohannesalemayehu624@gmail.com>
2024-08-21 23:14:01 +0300

    feat: finish task 22 lib/features/authentication/domain/usecases/logout.dart

--- mobile/eComerce_linter_clean_archtecture/lib/features/authentication/domain/usecases/logout.dart
+++ mobile/eComerce_linter_clean_archtecture/lib/features/authentication/domain/usecases/logout.dart
@@ -1,24 +1,15 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 
 import '../../../../core/error/failure.dart';
 import '../../../../core/usecase/usecase.dart';
 import '../repositories/auth_repository.dart';
 
-class LogoutUseCase implements UseCase<void, LogoutParams> {
+class LogoutUseCase implements UseCase<void, NoParams> {
   final AuthRepository authRepository;
   LogoutUseCase(this.authRepository);
 
   @override
-  Future<Either<Failure, void>> call(LogoutParams logoutParams) async {
-    return await authRepository.logout(logoutParams.userId);
+  Future<Either<Failure, void>> call(NoParams noParams) async {
+    return await authRepository.logout();
   }
-}
-
-class LogoutParams extends Equatable {
-  final String userId;
-
-  const LogoutParams({required this.userId});
-  @override
-  List<Object?> get props => [userId];
 }

8c7d408e2611274d5b452fcb6b6983cdd71fa77c
yohannesalex <yohannesalemayehu624@gmail.com>
2024-08-21 23:14:03 +0300

    feat: finish task 22 test/helpers/test_helper.dart

--- mobile/eComerce_linter_clean_archtecture/test/helpers/test_helper.dart
+++ mobile/eComerce_linter_clean_archtecture/test/helpers/test_helper.dart
@@ -2,6 +2,8 @@ import 'package:http/http.dart' as http;
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:my_project_name/core/network/network_info.dart';
+import 'package:my_project_name/features/authentication/data/resource/auth_local_data_source.dart';
+import 'package:my_project_name/features/authentication/data/resource/auth_remote_data_source.dart';
 import 'package:my_project_name/features/authentication/domain/repositories/auth_repository.dart';
 import 'package:my_project_name/features/eCommerce/data/data_sources/local_data_source.dart';
 import 'package:my_project_name/features/eCommerce/data/data_sources/remote_data_source.dart';
@@ -16,6 +18,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 @GenerateMocks(
   [
     AuthRepository,
+    AuthLocalDataSource,
+    AuthRemoteDataSource,
     ProductRepository,
     ProductRemoteDataSource,
     ProductLocalDataSource,

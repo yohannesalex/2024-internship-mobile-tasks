214007e66176b259ce2842fbdfbdefb20d77e066
yohannesalex <yohannesalemayehu624@gmail.com>
2024-08-21 23:14:02 +0300

    feat: finish task 22 test/features/authentication/domain/usecases/sign_up_test.dart

--- mobile/eComerce_linter_clean_archtecture/test/features/authentication/domain/usecases/sign_up_test.dart
+++ mobile/eComerce_linter_clean_archtecture/test/features/authentication/domain/usecases/sign_up_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:my_project_name/features/authentication/domain/entity/user.dart';
+import 'package:my_project_name/features/authentication/domain/entity/signup_entity.dart';
 import 'package:my_project_name/features/authentication/domain/usecases/sign_up.dart';
 
 import '../../../../helpers/test_helper.mocks.dart';
@@ -15,12 +15,8 @@ void main() {
     signUpUsecase = SignUpUseCase(mockAuthRepository);
   });
 
-  const testUserdetail = UserEntity(
-    id: '1',
-    userName: 'john',
-    email: 'abc@gmail.com',
-    password: 'abcd1234',
-  );
+  const testUserdetail =
+      SignUpEntity(name: 'john', email: 'abc@gmail.com', password: 'abcd1234');
 
   test('should sign up the user in the repository', () async {
     // Arrange
